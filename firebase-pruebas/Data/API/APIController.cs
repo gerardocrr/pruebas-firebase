be0ee65635e29e39f2ba406f1d130d47de1c6bbe
Gerardo Cruz <gerardo2551.cr@gmail.com>
2023-10-23 23:02:15 -0600

    problemas realtime

---
+++ firebase-pruebas/Data/API/APIController.cs
@@ -0,0 +1,22 @@
+﻿using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+
+namespace firebase_pruebas.Data.API
+{
+    public class APIController
+    {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "tR91FLbzKUIoulcdrBwVRLhe5oNdEKwu0Ft2zBrO",
+            BasePath = "https://crud-firebase-67d8a-default-rtdb.firebaseio.com/"
+        };
+
+        public IFirebaseClient client;
+
+        public APIController()
+        {
+            client = new FirebaseClient(config);
+        }
+    }
+}
---
+++ firebase-pruebas/Data/Models/Users.cs
@@ -0,0 +1,8 @@
+﻿namespace firebase_pruebas.Data.Models
+{
+    public class Users
+    {
+        public string user { get; set; }
+        public string password { get; set; }
+    }
+}
